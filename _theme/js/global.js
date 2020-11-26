319bc574b6617526c1a9ed458aa26b7cae8f815f
Akam Mohebbi <akamohebbi@gmail.com>
2020-11-26 17:33:17 +0330

    some code cleanups and little improvements

--- _theme/js/global.js
+++ _theme/js/global.js
@@ -1,9 +1,15 @@
----
-layout: none
----
+var tgl_header_nav_mobile = document.getElementById("tgl_header_nav_mobile");
+var page_header_nav_mobile = document.getElementById("page_header_nav_mobile");
 
-$(document).ready(function() {
-    $("#tgl_header_nav_mobile").click(function() {
-        $("#page_header_nav_mobile").fadeToggle();
-    });
-});
+const tgl_header_nav_mobile_CLASS_LIST = tgl_header_nav_mobile.classList;
+
+var is_showing_mobile_nav = false;
+tgl_header_nav_mobile.onclick = function() {
+    if (is_showing_mobile_nav) {
+        page_header_nav_mobile.style.display = "none";
+        is_showing_mobile_nav = false;
+    } else {
+        page_header_nav_mobile.style.display = "block";
+        is_showing_mobile_nav = true;
+    }
+};
